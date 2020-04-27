d30bf4f2809932b33a851f32a52f54a8ac40bcb6
pragatheeswaranshf <pragatheeswaran.s@happyfox.com>
2020-04-27 19:38:00 +0530

    Minor modification

--- addon-test-support/select-helpers/select-compare-options.js
+++ addon-test-support/select-helpers/select-compare-options.js
@@ -1,6 +1,5 @@
 import { click } from '@ember/test-helpers'
 import { getStringsArrayFromHtml } from '../utils';
-import customFind from '../dom-helpers/custom-find';
 
 export default async function selectCompareOptions(assert, selector, values, message = 'Choices are rendered correctly.', persistOrder = false) {
   await click(`${selector} .ember-power-select-trigger`);
@@ -10,5 +9,5 @@ export default async function selectCompareOptions(assert, selector, values, mes
   } else {
     assert.deepEqual(choices.sort(), values.sort(), message);
   }
-  return click(customFind(`${selector} .hf-pop-over_close-icon`)[0]);
+  return;
 }
