9c537f2da1c299f99c6c16a415dda66ba3c81064
pragatheeswaranshf <pragatheeswaran.s@happyfox.com>
2020-04-27 13:57:29 +0530

    Minor modifications

--- addon-test-support/dom-helpers/custom-trigger-event.js
+++ addon-test-support/dom-helpers/custom-trigger-event.js
@@ -1,6 +1,6 @@
 import { triggerEvent } from '@ember/test-helpers';
 import { getCustomQueryString } from '../utils';
 
-export default async function customTriggerEvent(selector, eventName) {
-  return triggerEvent(getCustomQueryString(selector), eventName);
+export default async function customTriggerEvent(selector, eventName, options = {}) {
+  return triggerEvent(getCustomQueryString(selector), eventName, options);
 }
