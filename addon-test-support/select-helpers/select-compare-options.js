48d22b8be4825bf8fb9f1a069ce5eb28108e2334
pragatheeswaranshf <pragatheeswaran.s@happyfox.com>
2020-04-27 19:16:56 +0530

    Minor modification

--- addon-test-support/select-helpers/select-compare-options.js
+++ addon-test-support/select-helpers/select-compare-options.js
@@ -2,13 +2,13 @@ import { click } from '@ember/test-helpers'
 import { getStringsArrayFromHtml } from '../utils';
 import customFind from '../dom-helpers/custom-find';
 
-export default async function selectCompareOptions(assert, jsTestClass, values, message = 'Choices are rendered correctly.', persistOrder = false) {
-  await click(`hf-js-test-${jsTestClass} .ember-power-select-trigger`);
-  const choices = getStringsArrayFromHtml(document.querySelectorAll(`.hf-js-test-${jsTestClass} .ember-power-select-option`));
+export default async function selectCompareOptions(assert, selector, values, message = 'Choices are rendered correctly.', persistOrder = false) {
+  await click(`${selector} .ember-power-select-trigger`);
+  const choices = getStringsArrayFromHtml(document.querySelectorAll(`${selector} .ember-power-select-option`));
   if (persistOrder) {
     assert.deepEqual(choices, values, message);
   } else {
     assert.deepEqual(choices.sort(), values.sort(), message);
   }
-  return click(customFind(`.hf-js-test-${jsTestClass} .hf-pop-over_close-icon`)[0]);
+  return click(customFind(`${selector} .hf-pop-over_close-icon`)[0]);
 }
