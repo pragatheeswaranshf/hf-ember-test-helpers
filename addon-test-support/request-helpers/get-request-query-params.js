fe5e401b9c3007da3a12c684c51fab2ef5137a90
pragatheeswaranshf <pragatheeswaran.s@happyfox.com>
2020-05-15 17:12:53 +0530

    Incorrect export fixed

--- addon-test-support/request-helpers/get-request-query-params.js
+++ addon-test-support/request-helpers/get-request-query-params.js
@@ -1,6 +1,6 @@
 import { getRequest } from './get-request';
 
-export default function getRequestBody() {
+export default function getRequestQueryParams() {
   const request = getRequest();
   return request.queryParams;
 }
